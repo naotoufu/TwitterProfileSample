e994b9e387d96f6304de399c831d7cbbfbaec7c0
NaotoIto <naoto_itou@waku-2.com>
2018-11-15 12:49:31 +0900

    [add] tap gesture

--- ScrollViewAutoLayoutSample/HeaderView.swift
+++ ScrollViewAutoLayoutSample/HeaderView.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class HeaderView: TouchTransparentView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            // handling code
+            print(#function)
+        }
+    }
+    
+    @objc func handlePan(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .ended {
+            // handling code
+            print(#function)
+        }
+    }
     
     override func awakeFromNib() {
         layer.borderWidth = 4
         layer.borderColor = UIColor.black.cgColor
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        imageView.addGestureRecognizer(tap)
+        imageView.addGestureRecognizer(pan)
     }
 
     @IBAction func tappedButton(_ sender: Any) {
---
+++ ScrollViewAutoLayoutSample/TouchTransparentStackView.swift
@@ -0,0 +1,28 @@
+//
+//  TouchTransparentStackView.swift
+//  ScrollViewAutoLayoutSample
+//
+//  Created by NaotoIto on 2018/11/15.
+//  Copyright © 2018年 NaotoIto. All rights reserved.
+//
+
+import UIKit
+
+class TouchTransparentStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override public func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        
+        guard hitView !== self else {
+            return nil
+        }
+        return hitView
+    }
+}
